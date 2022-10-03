a2482cf5401985dd75eb3bfa1d1aea356b791b6e
tedschlicke <59693840+tedschlicke@users.noreply.github.com>
2022-10-03 14:35:20 +0100

    Update getMajorAxis.m

--- MathsStuff/getMajorAxis.m
+++ MathsStuff/getMajorAxis.m
@@ -1,4 +1,13 @@
 function majorAxis = getMajorAxis(u,v)
+% Calculate major axis of ellipse based on u,v velocities
+% 
+% INPUTS:
+% u - x component of velocity
+% v - y compoent of velocity
+%
+% OUTPUT:
+% majorAxis (in degrees)
+
 % Need covariance for major axis:
 PCAParameters = RCM.Utils.PCA(u,v);
 f = PCAParameters.eigenVector(1,PCAParameters.cols(1));
