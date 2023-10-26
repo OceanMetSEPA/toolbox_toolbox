5a1cfdf4611e2cc83e8b9430d62c9d50b21a5425
tedschlicke <59693840+tedschlicke@users.noreply.github.com>
2023-10-26 09:29:58 +0100

    Add optional input to apply buffer before determining axis

--- PlotStuff/polyshape2axis.m
+++ PlotStuff/polyshape2axis.m
@@ -1,11 +1,18 @@
-function ax=polyshape2axis(ps)
+function ax=polyshape2axis(ps,buff)
 % For polyshape (or array thereof), find axis for tight plotting
 % Note that there is a boundary method in polyshape, but that still needs
 % to be tinkered with to get what we want. Here we just work with vertices.
 % INPUT:
-% polyshape(s)
+%   ps - matlab polyshape
+% Optional input:
+%   buff - apply this buffer to polyshape before determining axis
 % OUTPUT:
 % [1x4] array
+
+if nargin==2
+    ps=polybuffer(ps,buff);
+end
+
 vertices=vertcat(ps.Vertices);
 x=vertices(:,1);
 y=vertices(:,2);
