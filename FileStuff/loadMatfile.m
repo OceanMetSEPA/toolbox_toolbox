35816e5b1cd26dd99d3d6e586f9700828187912f
tedschlicke <59693840+tedschlicke@users.noreply.github.com>
2023-10-26 09:26:18 +0100

    Altered to allow multiple inputs

--- FileStuff/loadMatfile.m
+++ FileStuff/loadMatfile.m
@@ -5,7 +5,7 @@ function op=loadMatfile(matfileName)
 %
 % This function uses faster load function, then returns the struct field
 % rather than the struct
-%%
+%
 % matfileName='C:\MIKEZeroProjectsSEPA\FishTrajectory\sparseConcStructRaw.mat';
 % fprintf('Importdata:\n')
 % tic
@@ -18,7 +18,18 @@ function op=loadMatfile(matfileName)
 % toc % 10s
 % %
 % %isequal(imp,checkOrig.sparseConcStruct)
-%%  
+
+% 20231025 - modify so we can add multiple inputs with one call
+matfileName=cellstr(matfileName); % convert from char to cellstr
+if length(matfileName)>1 % more than 1 input?
+    op=cellfun(@loadMatfile,matfileName,'unif',0);
+    try % this works if inputs are of same format (e.g. structs with matching fields):
+        op=vertcat(op{:});
+    catch % oh well, never mind - just return cell array
+    end
+    return
+end
+matfileName=char(matfileName);
 data=load(matfileName);
 try
     fn=char(fieldnames(data));
