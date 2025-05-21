fb0c8b3a84eccf51a37d512d2141c5fe049dc46c
Schlicke <Ted.Schlicke@SEPA.org.uk>
2025-05-21 10:21:17 +0100

    Update in matlab

--- FileStuff/loadMatfile.m
+++ FileStuff/loadMatfile.m
@@ -1,5 +1,5 @@
 function op=loadMatfile(matfileName)
-% Load matfile
+% Load matfile(s)
 % Function 'load' loads matfile as struct, where the field is what we're actually we're interested in
 % Function 'importdata' does what we want, but is several times slower
 %
@@ -18,6 +18,7 @@ function op=loadMatfile(matfileName)
 % toc % 10s
 % %
 % %isequal(imp,checkOrig.sparseConcStruct)
+% 20250521 - minor change to comment for testing git clone/push
 
 % 20231025 - modify so we can add multiple inputs with one call
 matfileName=cellstr(matfileName); % convert from char to cellstr
