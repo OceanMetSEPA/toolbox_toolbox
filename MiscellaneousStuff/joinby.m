8af1b9f03024f0819ac366ca1689f4c9df5051f0
tedschlicke <59693840+tedschlicke@users.noreply.github.com>
2025-12-02 17:01:30 +0000

    Update joinby.m
    Fixed case where cell of length 1 passed

--- MiscellaneousStuff/joinby.m
+++ MiscellaneousStuff/joinby.m
@@ -1,27 +1,5 @@
 function joinedUp = joinby(x,joinBy )
 % join cells in cell array (argument 1), connected by 2nd argument
-%
-% INPUT:
-% x - cell array of row / column vectors
-% joinBy - object to connect x's cells by
-%
-% OUTPUT:
-% joinedUp - connected vector of cells / numeric values
-%
-% EXAMPLES:
-% joinby(splitby([1,2,3,NaN,4,5,6,7,NaN,8,9,10],NaN),-999)
-% joinby(splitby([1,2,3,NaN,4,5,6,7,NaN,8,9,10],NaN),'Hello')
-% joinby({[1,2,3],[7,8,9],[15,20,25,30]'},NaN)
-% joinby({[1,2,3];[7,8,9];[15,20,25,30]'},Inf)
-% joinby({1,2,3,'frog',pi},'FISH')
-% joinby({[1,2,3],'frog',pi},'hedgehog')
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% $Workfile:   joinby.m  $
-% $Revision:   1.1  $
-% $Author:   ted.schlicke  $
-% $Date:   Jul 31 2015 11:55:00  $
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 if nargin<2
     help joinby
@@ -42,22 +20,35 @@ if ~rowVector
 end
 
 Nx=length(x);
-for i=1:Nx % Loop through cells of cell array
+for i=1:Nx
     xi=x{i};
     if isnumeric(xi) || islogical(xi)
-        x{i}=num2cell(xi); % convert numeric/logical to cells. This means we can join by non-numeric/logical classes
+        x{i}=num2cell(xi);
     elseif ischar(xi)
         x{i}=cellstr(xi);
     end
 end
 
-joinedUp=arrayfun(@(i){[x{i},joinBy]},1:(Nx-1)); % Connect cells with 'joinBy' value
-joinedUp=[horzcat(joinedUp{:}),x{end}]; % combine row vectors with end bit
+% --- FIX: handle single-element case ------------------------------------
+if Nx == 1
+    joinedUp = x{1};  % nothing to join
+    try
+        joinedUp = cell2mat(joinedUp);
+    catch
+    end
+    if ~rowVector
+        joinedUp = joinedUp(:);
+    end
+    return;
+end
+% ------------------------------------------------------------------------
 
-try % Try to convert back to numeric
+joinedUp=arrayfun(@(i){[x{i},joinBy]},1:(Nx-1));
+joinedUp=[horzcat(joinedUp{:}),x{end}];
+
+try
     joinedUp=cell2mat(joinedUp);
 catch
-    % don't worry about it - leave as cell array
 end
 
 if ~rowVector
